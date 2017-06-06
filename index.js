63dbc90bd77a6c44632c769c917363b1798e1a21
Cameron Glass <glass10@purdue.edu>
2017-06-06 18:29:00 -0400

    Get form data and make card

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const personForm = document.querySelector('#personForm');
 personForm.addEventListener('submit', handleSubmit);
 
+const students= [];
+
 function handleSubmit(ev){
     ev.preventDefault();
     const f = ev.target;
-    console.log("we in here");
+    const studentName = f.name.value;
+    const studentImage = f.image.value;
+    
+    const student = {
+    name: studentName,
+    image: studentImage,
+    status: "OK"
+}
+
+    const list = document.createElement('p');
+
+    students.push(student);
+    for(var i = 0; i < students.length; i++){
+        console.log(students[i].name);
+        const name = students[i].name;
+        const image = students[i].image;
+        const item = document.createElement('img');
+        item.innerHTML = `<img src=${image} alt=${name} style="width:150px;height:150px;">`
+        list.appendChild(item);
+    }
+    grid.innerHTML = "";
+    grid.appendChild(list);
+
+    /*{<div class="card">
+                <img class="card-img" src="http://placehold.it/350x300" alt="header" />
+                <div class="card-info">
+                <h1 class="card-title">Card Title</h1>
+                <div class="card-icon"></div>
+                <p class="card-stats">6 <img src="http://placehold.it/20" alt="hi" /> 6 <img src="http://placehold.it/20" alt="hi" /></p>
+                </div>
+            </div>}*/
 
 }
