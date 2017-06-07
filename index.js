54fcfc9ee93dbb06ddc64fd3c02e370f5f6be76e
Cameron Glass <glass10@purdue.edu>
2017-06-06 21:02:12 -0400

    Make cards with functioning buttons

--- index.js
+++ index.js
@@ -3,7 +3,47 @@ personForm.addEventListener('submit', handleSubmit);
 
 const students= [];
 
+function okay(id){
+    const icon = document.getElementById(id+"-icon");
+
+    for(var i = 0; i < students.length; i++){
+        if(students[i].name === id){
+            students[i].status = "OK";
+        }
+    }
+
+    icon.style.backgroundColor = "LightGreen";
+    console.log("okay");
+}
+
+function late(id){
+    const icon = document.getElementById(id+"-icon");
+
+    for(var i = 0; i < students.length; i++){
+        if(students[i].name === id){
+            students[i].status = "LATE";
+        }
+    }
+
+    icon.style.backgroundColor = "Orange";
+    console.log("late");
+}
+
+function absent(id){
+    const icon = document.getElementById(id+"-icon");
+
+    for(var i = 0; i < students.length; i++){
+        if(students[i].name === id){
+            students[i].status = "ABSENT";
+        }
+    }
+
+    icon.style.backgroundColor = "Red";
+    console.log("absent");
+}
+
 function handleSubmit(ev){
+
     ev.preventDefault();
     const f = ev.target;
     const studentName = f.name.value;
@@ -15,27 +55,32 @@ function handleSubmit(ev){
     status: "OK"
 }
 
-    const list = document.createElement('p');
+    var d1 = document.getElementById('grid');
+    d1.innerHTML = "";
 
     students.push(student);
+
     for(var i = 0; i < students.length; i++){
         console.log(students[i].name);
         const name = students[i].name;
         const image = students[i].image;
-        const item = document.createElement('img');
-        item.innerHTML = `<img src=${image} alt=${name} style="width:150px;height:150px;">`
-        list.appendChild(item);
+        const status = students[i].status;
+        d1.insertAdjacentHTML('beforeend', createCard(name, image, name+"-icon", status));
     }
-    grid.innerHTML = "";
-    grid.appendChild(list);
 
-    /*{<div class="card">
-                <img class="card-img" src="http://placehold.it/350x300" alt="header" />
+}
+
+function createCard(name, image, icon, status){
+    const card = `
+        <div class="card">
+                <img class="card-img" src="https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg" alt="Profile" />
                 <div class="card-info">
-                <h1 class="card-title">Card Title</h1>
-                <div class="card-icon"></div>
-                <p class="card-stats">6 <img src="http://placehold.it/20" alt="hi" /> 6 <img src="http://placehold.it/20" alt="hi" /></p>
+                <h1 class="card-title">${name}</h1>
+                <div class="card-icon-${status}(" id="${icon}"></div>
+                <p class="card-buttons" id="${name}"><button type="button" id="ok" onClick="okay(this.parentNode.id)">Ok</button> <button type="button" id="late" onClick = late(this.parentNode.id)>Late</button> <button type="button" id="absent" onClick=absent(this.parentNode.id)>Absent</button></p>
                 </div>
-            </div>}*/
+            </div>
+    `;
 
+    return card;
 }
